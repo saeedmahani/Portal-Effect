a1f72cc72af9c998c80be62b7e14e64781c4ea4c
Saeed Mahani <smahani@nevion.com>
2012-10-22 20:59:00 -0700

    Initial Commit

---
+++ Portal Effect/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Portal Effect
+//
+//  Created by Saeed Mahani on 10/22/12.
+//  Copyright (c) 2012 Saeed Mahani. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
